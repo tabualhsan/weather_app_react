3388e27a38a83656e6850e3e22001807358a3528
Tamara <tabualhsan@gmail.com>
2021-08-02 07:50:23 -0700

    initilized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import axois from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [weather, setWeather] = useState(null)
+  useEffect(() => {
+
+    axois.get("http://api.weatherapi.com/v1/current.json?key=c9fd6d5b2a8b4ea5bf4200630213007&q=94015&aqi=no")
+    .then(data => {
+      setWeather(data.data);
+      console.log(data.data);
+    })
+    .catch(err => console.log(err));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    {weather && (
+    <div>
+    <h1> {weather.location.country}</h1>
+    <h2> {weather.location.region}</h2>
     </div>
+    )}
+
+</div>
   );
-}
+    }
 
 export default App;
